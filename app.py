68eeb31f5f0bd959ff62f5709f7f39aef0a9f43f
Chris Marsh <cmhilfiker@gmail.com>
2020-07-24 11:30:47 +0000

    Format code/pretify
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- app.py
+++ app.py
@@ -359,4 +359,4 @@ def error_500(error):
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')), debug=True)
+            port=int(os.environ.get('PORT')), debug=False)
